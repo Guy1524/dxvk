08e1abe743a280351e1c630e1b6fb51e578b4b95
Derek Lesho <dereklesho52@Gmail.com>
2018-11-15 21:39:25 -0500

    Add better COM support

--- include/native/unknwn.h
+++ include/native/unknwn.h
@@ -6,6 +6,8 @@
 #include <atomic>
 #include <inttypes.h>
 
+const GUID IUnknown_GUID = {0x00000000,0x0000,0x0000,{0xC0,0x00,0x00,0x00,0x00,0x00,0x00,0x46}};
+
 struct IUnknown
 {
     std::atomic<uint32_t> m_refCount;
@@ -36,6 +38,19 @@ struct IUnknown
         }
 
         return m_refCount;
+    }
+
+    virtual HRESULT QueryInterface(REFIID riid, void **ppvObject)
+    {
+        *ppvObject = nullptr;
+
+        if( riid == __uuidof(IUnknown))
+        {
+            *ppvObject = this;
+            return S_OK;
+        }
+
+        return E_NOINTERFACE;
     }
 };
 
--- include/native/windows.h
+++ include/native/windows.h
@@ -1,11 +1,43 @@
 #ifndef WINDOWS_H
 #define WINDOWS_H
 
-//barebones windows.h for compatibility
+#include <inttypes.h>
 
-//basic integer definitions
-typedef unsigned int  DWORD;
-typedef unsigned long ULONG;
+// barebones windows.h for compatibility
+
+// basic integer definitions
+typedef unsigned char  BYTE;
+typedef unsigned short WORD;
+typedef unsigned int   DWORD;
+typedef unsigned long  ULONG;
+typedef long           LONG;
+
+// errors
+typedef LONG HRESULT;
+
+#define S_OK          0
+#define E_NOINTERFACE 0x80004002
+
+// COM
+#define __uuidof(iface)  iface ## _GUID
+
+struct GUID{
+    DWORD Data1;
+    WORD  Data2;
+    WORD  Data3;
+    BYTE  Data4[8];
+
+    bool operator ==(const GUID& other) const
+    {
+        return this->Data1 == other.Data1
+            && this->Data2 == other.Data2
+            && this->Data3 == other.Data3
+            && (uint64_t)this->Data4 == (uint64_t)other.Data4;
+    
+    }
+};
+
+typedef const GUID& REFIID;
 
 // misc.
 typedef wchar_t WCHAR;
