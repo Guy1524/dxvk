21f9a7d3ad8b22456be66476e86646d8b6ac5570
Derek Lesho <dereklesho52@Gmail.com>
2018-12-02 16:31:35 -0500

    Only setup error-handling in dxgi's main

--- src/dxgi/dxgi_main.cpp
+++ src/dxgi/dxgi_main.cpp
@@ -1,11 +1,26 @@
 #include "dxgi_factory.h"
 #include "dxgi_include.h"
 
+#include <csignal>
+
 namespace dxvk {
   
+#ifdef DXVK_NATIVE
+    void error_callback(int code, const char* description)
+    {
+      Logger::err( str::format("GLFW3 error.  code=", code, " description=", description));
+
+      std::raise(SIGINT);
+    }
+#endif
+
   Logger Logger::s_instance("dxgi.log");
   
   HRESULT createDxgiFactory(UINT Flags, REFIID riid, void **ppFactory) {
+    #ifdef DXVK_NATIVE
+      glfwSetErrorCallback(error_callback);
+    #endif
+    
     Com<DxgiFactory> factory = new DxgiFactory(Flags);
     HRESULT hr = factory->QueryInterface(riid, ppFactory);
 
@@ -16,7 +31,9 @@ namespace dxvk {
   }
 }
 
+#ifndef DXVK_NATIVE
 extern "C" {
+#endif
   DLLEXPORT HRESULT __stdcall CreateDXGIFactory2(UINT Flags, REFIID riid, void **ppFactory) {
     dxvk::Logger::warn("CreateDXGIFactory2: Ignoring flags");
     return dxvk::createDxgiFactory(Flags, riid, ppFactory);
@@ -29,4 +46,6 @@ extern "C" {
   DLLEXPORT HRESULT __stdcall CreateDXGIFactory(REFIID riid, void **ppFactory) {
     return dxvk::createDxgiFactory(0, riid, ppFactory);
   }
+#ifndef DXVK_NATIVE
 }
+#endif
